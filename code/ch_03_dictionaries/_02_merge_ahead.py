dfd2dc2a29bbd3e383a39c703e476c953363bdb6
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-14 17:05:06 +0200

    final dict merge code.

--- code/ch_03_dictionaries/_02_merge_ahead.py
+++ code/ch_03_dictionaries/_02_merge_ahead.py
@@ -18,19 +18,24 @@ print("query: {}".format(query))
 print("post:  {}".format(post))
 
 # Non-pythonic procedural way
-# TODO:
 m1 = {}
+for k in query:
+    m1[k] = query[k]
+for k in post:
+    m1[k] = post[k]
+for k in route:
+    m1[k] = route[k]
 
 # Classic pythonic way:
-# TODO:
-m2 = {}
+m2 = query.copy()
+m2.update(post)
+m2.update(route)
 
 # Via dictionary comprehensions:
-# TODO:
-m3 = {}
+m3 = {k: v for d in [query, post, route] for k, v in d.items()}
 
 # Python 3.5+ pythonic way, warning crashes on Python <= 3.4:
-m4 = {}
+m4 = {**query, **post, **route}
 
 print(m1)
 print(m2)
