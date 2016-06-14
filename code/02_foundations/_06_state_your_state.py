85fb86484b43e1398e99a0d07f7d013c5df30d54
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-14 09:25:15 +0200

    starter for send code

---
+++ code/02_foundations/_06_state_your_state.py
@@ -0,0 +1,20 @@
+import time
+import sys
+
+
+def main():
+    confirm = input("Are you sure you want to format drive C: [yes, NO]? ")
+    if not confirm or confirm.lower() != 'yes':
+        print("Format cancelled!")
+        return
+
+    for _ in range(40):
+        time.sleep(.15)
+        print('.', end='')
+        sys.stdout.flush()
+    print()
+    print("Format completed successful. Enjoy the new hard drive space.")
+
+
+main()
+
