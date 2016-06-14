f89386c197824edda6f9ce57bebc15733c8f0105
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-14 09:24:40 +0200

    final string format code

--- code/02_foundations/_05_stringification.py
+++ code/02_foundations/_05_stringification.py
@@ -1,21 +1,26 @@
 name = 'Michael'
 age = 43
 
-# absolutely not pythonic:
+# Create the string "Hi, I'm Michael and I'm 43 years old."
+
 # crash: print("Hi, I'm " + name + " and I'm " + age + " years old.")
+
+# works, but not pythonic
 print("Hi, I'm " + name + " and I'm " + str(age) + " years old.")
 
-# closer, but not using modern features IMO
-# limitations: Only ints, strs, and doubles can be formatted
+
+# probably pythonic
 print("Hi, I'm %s and I'm %d years old." % (name, age))
 
 # pythonic
 print("Hi, I'm {} and I'm {} years old.".format(name, age))
-print("Hi, I'm {1} years old and my name is {0}, yeah {1} years.".format(name, age))
+print("Hi, I'm {1} years old and my name is {0}, yeah {1}.".format(name, age))
 
-# if you have a dictionary then this is still better:
 data = {'day': 'Saturday', 'office': 'Home office', 'other': 'UNUSED'}
+# print: On Saturday I was working in my Home office!
 print("On {day} I was working in my {office}!".format(**data))
 
-# finally, in Python 3.6, you can:
-# print(f"On {day} I was working in my {office}!")
+# In Python 3.6
+print("Hi, I'm {name} and I'm {age} years old.".format(name=name, age=age))
+# print(f"Hi, I'm {name} and I'm {age} years old.")
+
