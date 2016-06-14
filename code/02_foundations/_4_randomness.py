f7d76e803380c76ffad04acd613ae3707946e353
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-14 07:50:55 +0200

    final code for random items.

--- code/02_foundations/_4_randomness.py
+++ code/02_foundations/_4_randomness.py
@@ -3,4 +3,12 @@ import random
 letters = "abcdefghijklmnopqrstuvwxyz1234567890"
 
 # BAD: C-style
+index = random.randint(0, len(letters)-1)
+item = letters[index]
+print(item)
+
+# Pythonic version
+item = random.choice(letters)
+print(item)
+
 
