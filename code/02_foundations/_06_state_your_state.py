de50d996e4d05e8dc290f0c02700922c0a30c304
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-14 09:55:50 +0200

    final code for exit code code

--- code/02_foundations/_06_state_your_state.py
+++ code/02_foundations/_06_state_your_state.py
@@ -6,7 +6,7 @@ def main():
     confirm = input("Are you sure you want to format drive C: [yes, NO]? ")
     if not confirm or confirm.lower() != 'yes':
         print("Format cancelled!")
-        return
+        sys.exit(1)
 
     for _ in range(40):
         time.sleep(.15)
@@ -14,7 +14,6 @@ def main():
         sys.stdout.flush()
     print()
     print("Format completed successful. Enjoy the new hard drive space.")
-
 
 main()
 
