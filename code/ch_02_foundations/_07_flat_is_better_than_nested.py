b11fbff01413cb8307db68d273fdd051510b93af
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-14 09:59:32 +0200

    major rename (need non-numerical folders to import other modules)

---
+++ code/ch_02_foundations/_07_flat_is_better_than_nested.py
@@ -0,0 +1,47 @@
+# noinspection PyProtectedMember
+import ch_02_foundations._07_flat_support_file as s
+
+
+def main():
+    download_nested()
+    download_flat()
+
+
+def download_nested():
+    print("Let's try to download a file")
+    if s.check_download_url():
+        if s.check_network():
+            if s.check_dns():
+                if s.check_access_allowed():
+                    print("Sweet, we can download ...")
+                else:
+                    print("No access")
+            else:
+                print("No DNS")
+        else:
+            print("No network")
+    else:
+        print("Bad url")
+
+
+def download_flat():
+    print("Let's try to download a file")
+
+    if not s.check_download_url():
+        print("Bad url")
+        return
+    if not s.check_network():
+        print("No network")
+        return
+    if not s.check_dns():
+        print("No DNS")
+        return
+    if not s.check_access_allowed():
+        print("No access")
+        return
+
+    print("Sweet, we can download ...")
+
+
+if __name__ == '__main__':
+    main()
