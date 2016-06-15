a7ce3dd40fd4af21dde29e6b7f015c4d0414b319
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-15 10:39:04 +0200

    starter for safe item access.

--- code/ch_03_dictionaries/_04_get_some.py
+++ code/ch_03_dictionaries/_04_get_some.py
@@ -1,42 +1,25 @@
-from collections import defaultdict
-
 data = {"year": "2001", "country": "USA", "title": "Johnny 5", "duration": "119 min"}
 
 print('optimistic style')
-print("Year is {}".format(data['year']))
-# KeyError: 'rating'
-# print("Rating is {}".format(data['rating']))
+# TODO: good, bad
+
 print()
 print('pessimistic style')
-try:
-    print("Year is {}".format(data['year']))
-    print("Rating is {}".format(data['rating']))
-except:
-    pass  # never prints rating
+# TODO: try
+
 print()
 print('safety first style')
-if 'year' in data:
-    print("Year is {}".format(data['year']))
-
-if 'rating' in data:
-    print("Rating is {}".format(data['rating']))
-else:
-    print("Oh, there is no rating")
+# TODO: test good, bad
 print()
 
 print('accept None instead style')
-print("Year is {}".format(data.get('year')))
-print("Rating is {}".format(data.get('rating')))
+# TODO: both
 print()
 
 print('Explicit alternate value style')
-print("Year is {}".format(data.get('year', 0)))
-print("Rating is {}".format(data.get('rating', 'NO RATING')))
+# TODO: both via get
 print()
 
-data2 = defaultdict(lambda: "MISSING", data)
+# via defaultdict
 print('accept default value instead style')
-print("Year is {}".format(data2['year']))
-print("Rating is {}".format(data2['rating']))
 print()
-
