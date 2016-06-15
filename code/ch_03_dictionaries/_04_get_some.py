307b6b7f09d20e46e67c0822c093b0ab0408a278
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-15 10:59:56 +0200

    final code for safe access

--- code/ch_03_dictionaries/_04_get_some.py
+++ code/ch_03_dictionaries/_04_get_some.py
@@ -1,25 +1,42 @@
-data = {"year": "2001", "country": "USA", "title": "Johnny 5", "duration": "119 min"}
+from collections import defaultdict
+
+data = {"year": 2001, "country": "USA", "title": "Johnny 5", "duration": "119 min"}
 
 print('optimistic style')
-# TODO: good, bad
+print(data['year'])
+# print(data['rating']) # Crash!
 
 print()
 print('pessimistic style')
-# TODO: try
+try:
+    print(data['year'])
+    print(data['rating'])
+except Exception as x:
+    print("Oops! {}".format(x))
+
 
 print()
 print('safety first style')
-# TODO: test good, bad
+if 'year' in data:
+    print(data['year'])
+if 'rating' in data:
+    print(data['rating'])
+else:
+    print("Oh we didn't find a rating...")
 print()
 
 print('accept None instead style')
-# TODO: both
+print(data.get('year'))
+print(data.get('rating'))
 print()
 
 print('Explicit alternate value style')
-# TODO: both via get
+print(data.get('year', 0))
+print(data.get('rating', '***'))
 print()
 
-# via defaultdict
+data = defaultdict(lambda: "MISSING", data)
 print('accept default value instead style')
+print(data['year'])
+print(data['rating'])
 print()
