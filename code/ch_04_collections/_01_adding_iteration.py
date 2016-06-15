820aed3c9c490dc460b096c435a7269533386641
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-15 14:11:01 +0200

    final code for iteration and containment.

--- code/ch_04_collections/_01_adding_iteration.py
+++ code/ch_04_collections/_01_adding_iteration.py
@@ -5,6 +5,12 @@ class ShoppingCart:
     def add_item(self, it):
         self.items.append(it)
 
+    def __iter__(self):
+        sorted_items = sorted(self.items, key=lambda i: -i.price)
+        return sorted_items.__iter__()
+        # for i in sorted_items:
+        #     yield i
+
 
 class CartItem:
     def __init__(self, name, price):
@@ -19,3 +25,7 @@ cart.add_item(CartItem("iPhone", 699))
 
 # Can we for-in the cart?
 # what if it was to be sorted?
+
+print("Items in your cart.")
+for item in cart:
+    print(" * {} ${}".format(item.name, item.price))
--- code/ch_04_collections/_02_containment.py
+++ code/ch_04_collections/_02_containment.py
@@ -9,3 +9,15 @@ print("Dict: {}".format(nums_dict))
 n = int(input("Enter a number to test for small fibonacci: "))
 
 # is this number in the sequences above?
+print("{} in list".format(n) if n in nums_list else "not in list")
+print("{} in set".format(n) if n in nums_set else "not in set")
+print("{} in dict".format(n) if n in nums_dict else "not in dict")
+
+text = "Why did the multithreaded chicken cross the street? Other side to the get."
+
+word = input("Enter word to search text for...")
+# if word in text:
+if text.find(word) >= 0:
+    print("{} is in {}".format(word, text))
+else:
+    print("{} is NOT in {}".format(word, text))
