de21bb5b58f48e852a842ba2883a2e910584fa9d
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-15 11:48:53 +0200

    starter code for from json and back

--- code/ch_03_dictionaries/_06_to_json_and_back.py
+++ code/ch_03_dictionaries/_06_to_json_and_back.py
@@ -1,5 +1,3 @@
-import json
-
 movie_json = """
 {
 "Title":"Johnny 5",
@@ -8,11 +6,3 @@ movie_json = """
 "Country":"USA"
 }
 """
-
-movie_data = json.loads(movie_json)
-# print(movie_data)
-print("Movie title from static json:")
-print(' * ' + movie_data['Title'])
-print()
-print("Movie data to json:")
-print(json.dumps(movie_data))
