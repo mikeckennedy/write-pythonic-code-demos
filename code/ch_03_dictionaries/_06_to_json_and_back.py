2b9b7cb35fc296604c63e783f148a9c6ac34e2f6
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-15 12:28:26 +0200

    final code for to and from json

--- code/ch_03_dictionaries/_06_to_json_and_back.py
+++ code/ch_03_dictionaries/_06_to_json_and_back.py
@@ -1,3 +1,5 @@
+import json
+
 movie_json = """
 {
 "Title":"Johnny 5",
@@ -6,3 +8,20 @@ movie_json = """
 "Country":"USA"
 }
 """
+
+movie_data = json.loads(movie_json)
+print(type(movie_data), movie_data)
+
+print("The title is {}".format(movie_data.get('Title')))
+
+movie_json_text_2 = json.dumps(movie_data)
+print(type(movie_json_text_2), movie_json_text_2)
+
+# print(type(movie_json), movie_json)
+# md = {
+#     "Title":"Johnny 5",
+#     "Year":"2001",
+#     "Runtime":"119 min",
+#     "Country":"USA"
+#     }
+# print(type(md), md)
