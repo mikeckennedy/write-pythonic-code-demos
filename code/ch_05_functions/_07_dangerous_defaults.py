7fb24d137fa0351c5f2ab13ed4ed3092b33ff208
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-16 21:48:30 +0200

    starter code for dangerous default values.

---
+++ code/ch_05_functions/_07_dangerous_defaults.py
@@ -0,0 +1,40 @@
+def main():
+    a = add_items_bad("a", 3)
+    print(a)
+    add_items_bad("b", 2, a)
+    print(a)
+
+    # danger revealed here
+    d = add_items_bad("d", 4)
+    print(d)
+
+    print(id(a), id(d))
+    print("Try again")
+    a = add_items_good("a", 3)
+    print(a)
+    add_items_good("b", 2, a)
+    print(a)
+
+    # danger revealed here
+    d = add_items_good("d", 4)
+    print(d)
+
+    print(id(a), id(d))
+
+
+# notice even PyCharm knows... alt-enter fixes.
+def add_items_bad(name, times=1, lst=[]):
+    for _ in range(0, times):
+        lst.append(name)
+    return lst
+
+
+# Ah, better (TODO: Fix it)
+def add_items_good(name, times=1, lst=[]):
+    for _ in range(0, times):
+        lst.append(name)
+    return lst
+
+
+if __name__ == '__main__':
+    main()
