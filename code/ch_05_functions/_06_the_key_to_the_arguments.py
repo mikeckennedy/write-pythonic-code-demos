2a200afe6293560dfd9cfe70a52bd4809cfd7aee
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-16 21:48:17 +0200

    final code for transforming dictionaries into arguments.

--- code/ch_05_functions/_06_the_key_to_the_arguments.py
+++ code/ch_05_functions/_06_the_key_to_the_arguments.py
@@ -1,11 +1,20 @@
 # add arbitrary additional kw arguments
-def display_greeting(name, greeting='Hello', times=1):
+def display_greeting(name, greeting='Hello', times=1, **kwargs):
     times = max(1, times)
     for _ in range(0, times):
         print("{} {}!".format(greeting, name))
+    print("kwargs = {}".format(kwargs))
 
-display_greeting(greeting="Hey, you're out of order", name="Michael")
+display_greeting(greeting="Hey, you're out of order",
+                 name="Michael", additional=2, mode=7)
 
 print()
 
 # from dictionary
+data = {
+    'name': 'Ted',
+    'greeting': 'Long time no see bro!',
+    'times': 6
+}
+
+display_greeting(**data)
