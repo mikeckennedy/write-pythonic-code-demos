27923b930838cb6404ed7c8409ad829e72c514e2
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-16 11:34:53 +0200

    default args final.

--- code/ch_05_functions/_04_i_ll_have_my_usual.py
+++ code/ch_05_functions/_04_i_ll_have_my_usual.py
@@ -1,5 +1,5 @@
 # start with no defaults...
-def display_greeting(name, greeting, times):
+def display_greeting(name, greeting='Hello', times=1):
     times = max(1, times)
     for _ in range(0, times):
         print("{} {}!".format(greeting, name))
@@ -10,4 +10,7 @@ display_greeting("Michael", "G'day", 1)
 display_greeting("Mark")
 display_greeting("Mark", "Good afternoon")
 display_greeting("Mark", "Good afternoon", 2)
+
+display_greeting(greeting='Yo!', name='Michael', times=4)
+display_greeting('Michael', times=2)
 
