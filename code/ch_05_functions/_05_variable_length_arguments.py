38b95b9b2222f7c60f9322a0f4eea30260c81464
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-16 11:46:06 +0200

    variable num of args final

--- code/ch_05_functions/_05_variable_length_arguments.py
+++ code/ch_05_functions/_05_variable_length_arguments.py
@@ -7,4 +7,14 @@ def biggest(x, y):
 
 print(biggest(1, 7))
 
+
 # what about more than 2?
+def biggest(x, *args):
+    big = x
+    for y in args:
+        if y > big:
+            big = y
+    return big
+
+
+print(biggest(1, 7, 42, 99, -2, 11))
