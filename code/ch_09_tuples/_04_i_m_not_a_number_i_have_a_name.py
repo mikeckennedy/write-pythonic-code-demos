ecc236004f3e7de5ce4838d308de56edad060413
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-20 19:42:52 +0200

    final, named tuples.

--- code/ch_09_tuples/_04_i_m_not_a_number_i_have_a_name.py
+++ code/ch_09_tuples/_04_i_m_not_a_number_i_have_a_name.py
@@ -1,6 +1,29 @@
+import collections
+
+Rating = collections.namedtuple("Rating", "id, x, y, rating")
+
+
 def main():
-    for d in get_data_tricky():
-        print("id={}, rating={}, position=({}, {})".format(1, 1, 1, 1))
+    # for d in get_data_tricky():
+    #     print("id={}, rating={}, position=({}, {})".format(
+    #         d[0], d[3], d[2], d[1]))
+
+    for d in get_data_better():
+        print("id={}, rating={}, position=({}, {})".format(
+            d.id, d.rating, d.x, d.y))
+
+        _, _, x, y = d
+        print(x, y)
+
+
+def get_data_better():
+    data = [
+        Rating(1, 19.2, 11.1, 50),
+        Rating(2, 18.9, 12.0, 45),
+        Rating(3, 20.1, 14.0, 55),
+    ]
+
+    return data
 
 
 def get_data_tricky():
