3d57cc3d0156f00be4e218636140948b096a1027
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-20 13:12:40 +0200

    final code for loops part 1, starter code for part 2

--- code/ch_08_loops/_01_there_is_no_num_for_loop.py
+++ code/ch_08_loops/_01_there_is_no_num_for_loop.py
@@ -3,11 +3,20 @@
 # data = [1, 7, 11]
 # for i = 0; i<len(data); i++:
 #     print("Now i is " + i)
-#     print("The value is {}".format(data[i])
+#     print("The value is {}".format(data[i]))
 
 # Often new comers attempt to build one:
 
 data = [1, 7, 11]
-# faking it with a while
+
+# NOT pythonic: faking it with a while
+i = 0
+while i < len(data):
+    print("Now i is {}".format(i))
+    print("The value is {}".format(data[i]))
+    i += 1
+print()
 
 # don't be that person
+for item in data:
+    print("The value is {}".format(item))
---
+++ code/ch_08_loops/_02_oh_there_are_num_loops_rn.py
@@ -0,0 +1,10 @@
+# but sometimes, fairly rarely, you really need just an
+# increasing number of integers. 
+
+
+
+# think of the above as:
+# for i = 0; i<10; i++:
+#     print(i, end=', ')
+
+# So, use range to model for int loops
