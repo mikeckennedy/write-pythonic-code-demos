600785fe3f72c5c7dfbd52c1c1ce0530649676f3
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-20 17:40:08 +0200

    final code ref params in python

--- code/ch_09_tuples/_03_methods_with_ref_params.py
+++ code/ch_09_tuples/_03_methods_with_ref_params.py
@@ -4,11 +4,21 @@ import math
 def main():
     args = list()
     out_params_bad(7, args)
-    print("Return values (bad):  {} & {:.2f}".format(args[0], args[1]))
+    print("Return values (bad):   {} & {:.2f}".format(args[0], args[1]))
 
     # can we do better?
+    v1, v2 = out_params(7)
+    print("Return values (good):  {} & {:.2f}".format(v1, v2))
 
 
+# pythonic!
+def out_params(base: float):
+    r1 = base * base
+    r2 = math.sqrt(base * base * base)
+
+    return r1, r2
+
+# non-pythonic!
 def out_params_bad(base: float, args: list):
     if len(args) == 0:
         args.append(0)
