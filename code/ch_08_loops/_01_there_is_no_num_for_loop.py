dc2a8cee7425541fc947a5dac06d4c36e4a79069
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-20 11:13:08 +0200

    starter code for loops 1

---
+++ code/ch_08_loops/_01_there_is_no_num_for_loop.py
@@ -0,0 +1,13 @@
+# There is no such concept in Python:
+
+# data = [1, 7, 11]
+# for i = 0; i<len(data); i++:
+#     print("Now i is " + i)
+#     print("The value is {}".format(data[i])
+
+# Often new comers attempt to build one:
+
+data = [1, 7, 11]
+# faking it with a while
+
+# don't be that person
