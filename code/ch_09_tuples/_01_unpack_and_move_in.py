3835c0c2c10b45480960c88bb7b83be12cb94d77
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-20 17:01:31 +0200

    final code for swap and unpack

--- code/ch_09_tuples/_01_unpack_and_move_in.py
+++ code/ch_09_tuples/_01_unpack_and_move_in.py
@@ -1,11 +1,27 @@
 # tuples are defined as:
+t = (7, 11, "cat", [1, 1, 3, 5, 8])
+print(t)
+t = 7, 11, "cat", [1, 1, 3, 5, 8]
+# print(t)
+# t = 7,
+# print(t, len(t))
 
 # create a tuple, grab a value.
+print(t[2])
 
 # we can assign individual variables:
+t = 7, "cat", 11
+# n = [0]
+# a = [1]
 
 # show them
+n, a, _ = t
+print("n={}, a={}".format(n, a))
 
 # can also assign on a single line:
+x, y = 1, 2
+print(x, y)
 
 # You'll find this often in loops (remember numerical for-in loops):
+for idx, item in enumerate(['hat', 'cat', 'mat', 'that']):
+    print("{} -> {}".format(idx, item))
---
+++ code/ch_09_tuples/_02_short_swap_tricks.py
@@ -0,0 +1,13 @@
+x = 7
+y = 11
+
+print("x={}, y={}".format(x, y))
+
+# swap: nonpythonic
+# temp = x
+# x = y
+# y = temp
+
+y, x = x, y
+
+print("x={}, y={}".format(x, y))
