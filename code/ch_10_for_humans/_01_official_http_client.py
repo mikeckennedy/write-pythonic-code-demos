3c15b34b43f53850decb5af454579aab237a817d
Michael Kennedy <mikeckennedy@gmail.com>
2016-06-21 08:44:03 +0200

    final code for humans.

--- code/ch_10_for_humans/_01_official_http_client.py
+++ code/ch_10_for_humans/_01_official_http_client.py
@@ -1,6 +1,15 @@
+import requests
 
 # from: http://www.omdbapi.com/
 title_text = input("Enter a title search string: ")
 url = 'http://www.omdbapi.com/?y=&plot=short&r=json&s={}'.format(title_text)
 
 # process json-> Search -> Title
+resp = requests.get(url)
+if resp.status_code != 200:
+    print("Whoa, status code unexpected! {}".format(resp.status_code))
+else:
+    data = resp.json()
+    search = data['Search']
+    for m in search:
+        print("* {}".format(m['Title']))
--- code/ch_10_for_humans/_02_just_write_sql.py
+++ code/ch_10_for_humans/_02_just_write_sql.py
@@ -1,7 +1,12 @@
-
+import records
 # noinspection PyProtectedMember
 from ch_10_for_humans._02_just_write_sql_support import conn_str
 
 print(conn_str)
 
-# find high measurements via records
+db = records.Database(conn_str)
+rows = db.query("SELECT * FROM Measurement WHERE value > .9 "
+                "ORDER BY value DESC")
+
+for r in rows[:5]:
+    print(r)
