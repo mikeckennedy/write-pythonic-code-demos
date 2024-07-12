e7eabcc71e223107f8dbf41e20eb93cd830d4f6a
Michael Kennedy <michael@talkpython.fm>
2024-07-12 14:38:53 -0700

    start code for flat

--- code/ch_02_foundations/_07_flat_is_better_than_nested.py
+++ code/ch_02_foundations/_07_flat_is_better_than_nested.py
@@ -4,27 +4,7 @@ import _07_flat_support_file as s
 
 def main():
     download_nested()
-    download_flat()
-
-
-def download_flat():
-    print("Let's try to download a file")
-    if not s.check_download_url():
-        print("Bad url")
-        return
-
-    if not s.check_network():
-        print("No network")
-        return
-
-    if not s.check_dns():
-        print("No DNS")
-        return
-    if not s.check_access_allowed():
-        print("No access")
-        return
-
-    print("Sweet, we can download ...")
+    # download_flat()
 
 
 def download_nested():
