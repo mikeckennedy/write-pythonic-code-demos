63e24ca3b16be6149a67c6658bb530d44d1eb85b
Michael Kennedy <michael@talkpython.fm>
2024-07-12 14:20:25 -0700

    start for strings

--- code/ch_02_foundations/_05_stringification.py
+++ code/ch_02_foundations/_05_stringification.py
@@ -1,26 +1,25 @@
 name = 'Michael'
-age = 43
+age = 50
 
-# Create the string "Hi, I'm Michael and I'm 43 years old."
+# Create the string "Hi, I'm Michael, and I'm 50 years old."
 
-# crash: print("Hi, I'm " + name + " and I'm " + age + " years old.")
+# crash: print("Hi, I'm " + name + ", and I'm " + age + " years old.")
 
 # works, but not pythonic
-print("Hi, I'm " + name + " and I'm " + str(age) + " years old.")
+print("Hi, I'm " + name + ", and I'm " + str(age) + " years old.")
 
 
 # probably pythonic
-print("Hi, I'm %s and I'm %d years old." % (name, age))
+print("Hi, I'm %s, and I'm %d years old." % (name, age))
 
 # pythonic
-print("Hi, I'm {} and I'm {} years old.".format(name, age))
-print("Hi, I'm {1} years old and my name is {0}, yeah {1}.".format(name, age))
+print("Hi, I'm {}, and I'm {} years old.".format(name, age))
+print("Hi, I'm {1}, years old and my name is {0}, yeah {1}.".format(name, age))
 
 data = {'day': 'Saturday', 'office': 'Home office', 'other': 'UNUSED'}
 # print: On Saturday I was working in my Home office!
 print("On {day} I was working in my {office}!".format(**data))
 
-# In Python 3.6
-print("Hi, I'm {name} and I'm {age} years old.".format(name=name, age=age))
-# print(f"Hi, I'm {name} and I'm {age} years old.")
+# In Python 3.6+
+print(f"Hi, I'm {name}, and I'm {age} years old.")
 
