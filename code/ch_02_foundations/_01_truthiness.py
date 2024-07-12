5d9a6851b6d550eed26c7a66e0f4f5af0bc7fc2f
Michael Kennedy <michael@talkpython.fm>
2024-07-12 11:55:20 -0700

    starter code for truthiness.

--- code/ch_02_foundations/_01_truthiness.py
+++ code/ch_02_foundations/_01_truthiness.py
@@ -3,43 +3,23 @@
 # ###############################
 
 
-def print_truthiness(msg, exp):
-    print(("TRUE" if exp else "FALSE") + " <-- " + msg)
+def print_truthiness(exp):
+    print(("TRUE" if exp else "FALSE") + f" <-- {exp}")
 
 
-print_truthiness("Testing True", True)
-print_truthiness("Testing False", False)
-# for sequences
+# bools
 
-seq = []
-print_truthiness("Empty list", seq)
-seq.append("The cat")
-print_truthiness("1 item list", seq)
+# sequences
 
-# for objects and numbers
-print_truthiness("Zero", 0)
-print_truthiness("Eleven", 11)
-print_truthiness("-Eleven", -11)
+# objects and numbers
 
-# for None
-print_truthiness("For none", None)
+# None-ness
 
 
 # custom types
-class AClass:
+class TruthableClass:
     def __init__(self):
         self.data = []
 
     def add(self, item):
         self.data.append(item)
-
-    def __bool__(self):
-        return True if self.data else False
-
-a = AClass()
-
-print_truthiness("Empty AClass", a)
-a.add("Thing")
-print_truthiness("nonempty AClass", a)
-
-
