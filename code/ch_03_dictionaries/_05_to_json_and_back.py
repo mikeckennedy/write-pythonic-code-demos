cd89d9ed12feca7578f4cfcf41428cf7da58ebe2
Michael Kennedy <michael@talkpython.fm>
2024-07-16 16:37:46 -0700

    json final code.

--- code/ch_03_dictionaries/_05_to_json_and_back.py
+++ code/ch_03_dictionaries/_05_to_json_and_back.py
@@ -2,19 +2,30 @@ import json
 
 movie_json = """
 {
-"Title":"Johnny 5",
-"Year":"2001",
-"Runtime":"119 min",
-"Country":"USA"
-}
-"""
+    "title": "Johnny 5",
+    "year": 2001,
+    "runtime": "119 min",
+    "country": "USA"
+}""".strip()
+
+# If it were a file
+# with open('some-file', 'r', encoding='utf-8') as fin:
+#     movie_data = json.load(fin)
 
 # Get movie data from string
+movie_data = json.loads(movie_json)
+movie_data['year'] = "2012"
 
 # What's the title?
+print(type(movie_data), type(movie_json))
+print(movie_data['title'])
+print(movie_data.get('title'))
+print(movie_data.get('title', "NO_TITLE"))
 
 # Back to JSON
-
+movie_json_generated = json.dumps(movie_data, indent=2)
+print(movie_json_generated)
+print(movie_json == movie_json_generated)
 #
 #
 #
