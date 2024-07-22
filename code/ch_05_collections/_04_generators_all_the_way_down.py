ae5d84158d04da000f22e8af0b63bb3879ee9969
Michael Kennedy <michael@talkpython.fm>
2024-07-22 15:56:03 -0700

    hierarchical generators.

--- code/ch_05_collections/_04_generators_all_the_way_down.py
+++ code/ch_05_collections/_04_generators_all_the_way_down.py
@@ -1,8 +1,21 @@
+from pathlib import Path
+from typing import Iterator
 
 
 def main():
-    root_dir = '/Users/michaelkennedy/Desktop/pythonic-course/transcripts/'
-    # Let's list the files in that folder using generators. 
+    root_dir = Path('/Users/michaelkennedy/Desktop/pythonic-course/transcripts/')
+    # Let's list the files in that folder using generators.
+    print("Searching for files in dir:")
+    for file in get_files(root_dir):
+        print(file.as_posix())
+
+
+def get_files(folder: Path) -> Iterator[Path]:
+    for item in sorted(folder.iterdir(), key=lambda f: f.as_posix()):
+        if item.is_dir():
+            yield from get_files(item)
+        elif item.is_file():
+            yield item
 
 
 if __name__ == '__main__':
