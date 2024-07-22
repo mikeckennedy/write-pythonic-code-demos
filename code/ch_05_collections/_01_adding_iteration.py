a3161e6922b5c54635bf1918e95a098a9c0a7c5b
Michael Kennedy <michael@talkpython.fm>
2024-07-22 14:58:54 -0700

    Adding iteration to your own types.

--- code/ch_05_collections/_01_adding_iteration.py
+++ code/ch_05_collections/_01_adding_iteration.py
@@ -1,9 +1,4 @@
-class ShoppingCart:
-    def __init__(self):
-        self.items = []
-
-    def add_item(self, it):
-        self.items.append(it)
+from typing import Iterator
 
 
 class CartItem:
@@ -12,14 +7,25 @@ class CartItem:
         self.name = name
 
 
+class ShoppingCart:
+    def __init__(self):
+        self.items: list[CartItem] = []
+
+    def add_item(self, it: CartItem):
+        self.items.append(it)
+
+    def __iter__(self) -> Iterator[CartItem]:
+        return iter(self.items)
+
+
 cart = ShoppingCart()
 cart.add_item(CartItem("guitar", 799))
 cart.add_item(CartItem("cd", 19))
-cart.add_item(CartItem("iPhone", 699))
+cart.add_item(CartItem("iPhone", 1699))
 
 # Can we for-in the cart?
 # what if it was to be sorted?
 
 print("Items in your cart.")
 for item in cart:
-    print(f" * {item.name} ${item.price}")
+    print(f" * {item.name} ${item.price:,}")
