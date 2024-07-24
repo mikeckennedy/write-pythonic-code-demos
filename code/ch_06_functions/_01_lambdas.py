3742c755c1a3f265a005b203a6be1623261453cc
Michael Kennedy <michael@talkpython.fm>
2024-07-24 13:06:50 -0700

    Sorting with lambdas code.

--- code/ch_06_functions/_01_lambdas.py
+++ code/ch_06_functions/_01_lambdas.py
@@ -2,6 +2,7 @@
 # Create by Michael Kennedy (@mkennedy)
 
 import collections
+import typing
 import uuid
 
 Measurement = collections.namedtuple('Measurement', 'id x y value')
@@ -9,12 +10,16 @@ Measurement = collections.namedtuple('Measurement', 'id x y value')
 
 def main():
     print("Find odd numbers via method:")
-    # TODO: Use find_special_numbers() for 25 odds
+    # Use find_special_numbers() for 25 odds
+    print(find_special_numbers(is_odd, limit=25))
     print()
 
     print("Find divisible by 6 via lambda:")
-    # TODO: Use find_special_numbers() for 25 n*6 numbers via Lambdas
+    # Use find_special_numbers() for 25 n*6 numbers via Lambdas
+    print(find_special_numbers(lambda n: n % 6 == 0, limit=25))
     print()
+
+    # df.do_thing(lambda n: n).do_thing2(lambda x: x)
 
     print("Sorted list of words: ")
     list_of_words = ['CPython', 'read', 'improvements,', 'issues.', 'on', 'comprehensive', 'porting', 'potential',
@@ -22,19 +27,30 @@ def main():
                      'including', 'and', 'Please', 'many', 'list']
 
     # Can we just call sort on the list?
-    # TODO: Sort list
+    list_of_words.sort()
+    print("Bad sort", list_of_words)
+
     # More control here.
-    # TODO: Sort better
+    # Sort better
+    list_of_words.sort(key=lambda word: word.lower())
+    print("Good sort", list_of_words)
+    # Sort size
+    list_of_words.sort(key=lambda word: len(word))
+    print("Size sort", list_of_words)
     print()
 
     # More complex sort of measurements
-    # TODO: Sort by value
-    # TODO: Sort by value and distance
+    # Sort by value
+    m2 = sorted(measurements, key=lambda m: m.value, reverse=True)
+    print("sorted by value", m2)
+    # Sort by value and distance
+    m3 = sorted(measurements, key=lambda m: (m.value, m.x*m.x + m.y+m.y), reverse=True)
+    print("sorted by value and distance", m3)
 
     print("Done")
 
 
-def find_special_numbers(special_selector, limit=10):
+def find_special_numbers(special_selector: typing.Callable[[int], bool], limit: int = 10):
     found = []
     n = 0
     while len(found) < limit:
@@ -44,11 +60,16 @@ def find_special_numbers(special_selector, limit=10):
     return found
 
 
+def is_odd(number: int) -> bool:
+    return number % 2 != 0
+
+
 measurements = [
     Measurement(str(uuid.uuid4()), 1, 1, 72),
     Measurement(str(uuid.uuid4()), 2, 1, 40),
     Measurement(str(uuid.uuid4()), 3, 1, 11),
     Measurement(str(uuid.uuid4()), 2, 1, 90),
+    Measurement(str(uuid.uuid4()), 5, 5, 90),
     Measurement(str(uuid.uuid4()), 2, 2, 60),
     Measurement(str(uuid.uuid4()), 2, 3, 73),
     Measurement(str(uuid.uuid4()), 3, 1, 40),
