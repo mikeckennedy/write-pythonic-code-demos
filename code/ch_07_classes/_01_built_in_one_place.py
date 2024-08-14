843b2167f6c9acdb81c2c3f6925d0aec060d4a2a
Michael Kennedy <michael@talkpython.fm>
2024-08-14 10:33:13 -0700

    starter for fields on classes

--- code/ch_07_classes/_01_built_in_one_place.py
+++ code/ch_07_classes/_01_built_in_one_place.py
@@ -1,23 +1,10 @@
 class NotSoPythonicPet:
-    def __init__(self, name, age):
-        self.age = age
+    def __init__(self, name):
         self.name = name
 
-    # self.name = None
-    # self.age = 0
-
-    # def set_name(self, name):
-    #     self.name = name
-    #
-    # def set_age(self, age):
-    #     self.age = age
-
     def __str__(self):
-        return f"A pet whose name is {self.name} and age is {self.age}."
+        return f"A pet whose name is {self.name}."
 
 
-cow = NotSoPythonicPet('betsy', 7)
-# cow.set_name('betsy')
-# cow.set_age(7)
-# cow.happiness = 11
+cow = NotSoPythonicPet('betsy')
 print(cow)
