3d836dd7c908faa3bf30886b11f375b29cd1a1ae
Michael Kennedy <michael@talkpython.fm>
2024-08-14 10:52:57 -0700

    final for static vs instance

--- code/ch_07_classes/_02_static_vs_instance.py
+++ code/ch_07_classes/_02_static_vs_instance.py
@@ -1,24 +1,20 @@
-class PythonicPet:
-    def __init__(self, name: str, age: int = 0):
-        # Make sure all the fields are defined in the initializer:
-        self.age = age
-        self.name = name
-        self.lifecycle = 'baby'
-
-        if age > 0:
-            self.lifecycle = "mid"
-
-    def set_age(self, age: int):
-        if age < 0 or age > 300:
-            raise Exception("No way a pet is that age!")
-
-        self.age = age
+class PythonicPetStaticStyle:
+    age: int = 0
+    name: str = ''
+    lifecycle: str = 'baby'
 
     def __str__(self):
         return f"A pet whose name is {self.name} whose age is {self.age} years old."
 
 
-cow = PythonicPet('Betsy')
-# cow.age = 11 # No!
-# cow.set_age(7)
+cow = PythonicPetStaticStyle()
+cow.name = 'Static Betsy'
 print(cow)
+
+print(f"Static value: {PythonicPetStaticStyle.name}")
+PythonicPetStaticStyle.name = 'Default name'
+print(f"Static value: {PythonicPetStaticStyle.name}")
+cow2 = PythonicPetStaticStyle()
+print(cow2)
+cow2.name = 'New cow!'
+print(cow2)
